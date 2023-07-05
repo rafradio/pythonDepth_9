ef773bf35cf08b31bbe1326881b762b8d90e1065
Rafail Abdyushev <rafaelab@mail.ru>
2023-07-05 15:24:18 +0300

    print class

---
+++ ClassForPrint.py
@@ -0,0 +1,19 @@
+import os
+import csv
+
+class ClassForPrint:
+    
+    def __init__(self):
+        self.PATH_TO_FILE = os.path.join(os.getcwd(), 'DumpedFiles', 'data.csv')
+
+    def dumpToCsv(self, func):
+        
+        def wrapNew(*args):
+            print(func.__name__)
+            csv_data = func(*args)
+            with open(self.PATH_TO_FILE, 'w', newline='', encoding='utf-8') as f_write:
+                writer = csv.writer(f_write, dialect='excel', quotechar='"', quoting=csv.QUOTE_ALL)
+                writer.writerows(csv_data)
+            return csv_data
+            
+        return wrapNew
